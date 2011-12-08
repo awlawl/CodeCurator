54f5e10e2f842e0e0b5448861c60a3fc1d48878e
Allen Lyons <allen.lyons@gmail.com>
2011-12-08 13:37:57 -0500

    Converted from tables to div

--- src/CuratorWebSearch/Scripts/SolrUrl.js
+++ src/CuratorWebSearch/Scripts/SolrUrl.js
@@ -1,3 +1,3 @@
-﻿//var _solrHost = "http://localhost:8983/solr";
-var _solrHost = "http://and:8989/solr-example"
+﻿var _solrHost = "http://localhost:8983/solr";
+//var _solrHost = "http://and:8989/solr-example"
 //var _solrHost = "http://eavdevcsearch01:8080/solr"
